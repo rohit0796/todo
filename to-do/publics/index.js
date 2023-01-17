12a747c395de49888520aa9557ea6ec08881991f
rohit0796 <91247476+rohit0796@users.noreply.github.com>
2023-01-17 20:37:56 +0530

    Update index.js

--- to-do/publics/index.js
+++ to-do/publics/index.js
@@ -13,6 +13,7 @@ app.use(require('./routes/router'))
 app.use(require('./routes/todos'))
 
 
-app.listen(3000,()=>{
-    console.log('istening on port 3000')
-})
+const PORT= process.env.PORT|| 5000
+app.listen(PORT, () => {
+    console.log("listening tp 5000")
+})  
